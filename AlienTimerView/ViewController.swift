670546e1313268d198b7a06ab1061f74db6c96c6
D7703_17 <d7703_17@d7703-17ui-imac.local>
2018-04-05 11:17:29 +0900

    AlienTimerView

--- AlienTimerView/ViewController.swift
+++ AlienTimerView/ViewController.swift
@@ -9,17 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+      
+      var counter = 1
+      // Timer 객체 생성
+      var myTimer = Timer()
+      
+      @IBOutlet weak var myImageView: UIImageView!
+      @IBOutlet weak var imageCounter: UILabel!
+      
       override func viewDidLoad() {
             super.viewDidLoad()
-            // Do any additional setup after loading the view, typically from a nib.
+            imageCounter.text = String(counter)
       }
-
-      override func didReceiveMemoryWarning() {
-            super.didReceiveMemoryWarning()
-            // Dispose of any resources that can be recreated.
+      
+      
+      @IBAction func play(_ sender: Any) {
+            // Timer 실행
+            myTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(doAnimation), userInfo: nil, repeats: true)
       }
-
-
+      
+      @IBAction func stop(_ sender: Any) {
+            // Timer를 중지
+            myTimer.invalidate()
+      }
+      
+      // Timer에 의해 동적으로 호출되는 함수
+      @objc func doAnimation() {
+            if counter == 5 {
+                  counter = 1
+            } else {
+                  counter = counter + 1
+            }
+            
+            myImageView.image = UIImage(named: "frame\(counter).png")
+            imageCounter.text = String(counter)
+      }
 }
-
